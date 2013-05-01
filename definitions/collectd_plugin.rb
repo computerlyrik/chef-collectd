f4df826605af985b1ff8b87c4c6fc5850a27350e
Christian Fischer <sw-dev@computerlyrik.de>
2013-05-01 20:09:29 +0200

    fixed: definition parameters check made work on ubuntu precise

--- definitions/collectd_plugin.rb
+++ definitions/collectd_plugin.rb
@@ -22,7 +22,7 @@ define :collectd_plugin, :options => {}, :template => nil, :cookbook => nil do
     owner "root"
     group "root"
     mode "644"
-    if params[:template].nil?
+    if params[:template].nil? || params[:template].empty?
       source "plugin.conf.erb"
       cookbook params[:cookbook] || "collectd"
     else
@@ -45,7 +45,7 @@ define :collectd_python_plugin, :options => {}, :module => nil, :path => nil do
     end
     retry
   end
-  if not params[:path].nil?
+  if not params[:path].blank?
     t.variables[:options][:paths] << params[:path]
   end
   t.variables[:options][:modules][params[:module] || params[:name]] = params[:options]
